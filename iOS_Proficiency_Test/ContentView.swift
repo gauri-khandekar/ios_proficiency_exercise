85ce39cf61349d080c12afa4b7ad1536ad177d4d
Gauri <gauri.khandekar@infosys.com>
2019-12-23 18:14:06 +0530

    Initial Commit

---
+++ iOS_Proficiency_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Proficiency_Test
+//
+//  Created by test on 23/12/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
